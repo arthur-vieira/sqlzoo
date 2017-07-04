99cf95fafa094b92451d7a6b780e3d076ef368c8
Qiao Zhang <qiao.zhang.qz@gmail.com>
2017-07-04 13:28:09 -0400

    Update 7-MORE-JOIN.sql
    1. Deleted question 4 and updated the question order
        --4. What are the titles of the films with id 11768, 11955, 21191
        SELECT title FROM movie
    	WHERE id IN (11768, 11955, 21191);
    2. Updated the syntax of question 12 and 15, using JOIN instead of nested SELECT to make the code more readable
    3. Added secondary key on GROUP BY phrase in question 13 and 14 to get rid of the syntax error by current engine used by SQL Zoo

--- 7-MORE-JOIN.sql
+++ 7-MORE-JOIN.sql
@@ -14,51 +14,46 @@ SELECT id, title, yr FROM movie
 	WHERE title LIKE 'Star Trek%'
 		ORDER BY yr;
 
---4. What are the titles of the films with id 11768, 11955, 21191
-
-SELECT title FROM movie
-	WHERE id IN (11768, 11955, 21191);
-
---5. What id number does the actress 'Glenn Close' have?
+--4. What id number does the actress 'Glenn Close' have?
 
 SELECT id FROM actor
 	WHERE name = 'Glenn Close';
 
---6. What is the id of the film 'Casablanca'
+--5. What is the id of the film 'Casablanca'
 
 SELECT id FROM movie
 	WHERE title = 'Casablanca';
 
---7. Obtain the cast list for 'Casablanca'. Use movieid=11768, this is the value that you obtained in the previous question.
+--6. Obtain the cast list for 'Casablanca'. Use movieid=11768, this is the value that you obtained in the previous question.
 
 SELECT name FROM actor JOIN casting ON id=actorid
 	WHERE movieid = 11768;
 
---8. Obtain the cast list for the film 'Alien'
+--7. Obtain the cast list for the film 'Alien'
 
 SELECT name FROM actor JOIN casting ON id=actorid
 	WHERE movieid = 
 		(SELECT id FROM movie WHERE title = 'Alien');
 
---9. List the films in which 'Harrison Ford' has appeared
+--8. List the films in which 'Harrison Ford' has appeared
 
 SELECT title FROM movie JOIN casting ON id=movieid
 	WHERE actorid = 
 		(SELECT id FROM actor WHERE name = 'Harrison Ford');
 
---10. List the films where 'Harrison Ford' has appeared - but not in the starring role.
+--9. List the films where 'Harrison Ford' has appeared - but not in the starring role.
 
 SELECT title FROM movie JOIN casting ON id=movieid
 	WHERE ord > 1 AND actorid = 
 		(SELECT id FROM actor WHERE name = 'Harrison Ford');
 
---11. List the films together with the leading star for all 1962 films.
+--10. List the films together with the leading star for all 1962 films.
 
 SELECT title, name FROM movie JOIN casting ON id=movieid
 	JOIN actor ON actor.id=actorid
 		WHERE ord = 1 AND yr = 1962;
 
---12. Which were the busiest years for 'John Travolta', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+--11. Which were the busiest years for 'John Travolta', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
 
 SELECT yr,COUNT(title) FROM movie JOIN casting ON movie.id=movieid
 	JOIN actor ON actorid=actor.id
@@ -66,36 +61,35 @@ SELECT yr,COUNT(title) FROM movie JOIN casting ON movie.id=movieid
 			GROUP BY yr
 				HAVING COUNT(title) > 2;
 
---13. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+--12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
 
-SELECT title, name FROM movie JOIN casting ON movie.id=movieid
+SELECT title, name FROM casting JOIN movie ON movieid=movie.id 
 	JOIN actor ON actorid=actor.id
-		WHERE movie.id IN 
-			(SELECT movieid FROM casting WHERE actorid = 
-				(SELECT actor.id FROM actor WHERE name='Julie Andrews')
-			)
-			AND ord = 1;
+		WHERE movie.id IN (
+ 			SELECT movieid FROM actor JOIN casting ON actorid=id
+  				WHERE name='Julie Andrews') 
+					AND ord = 1
 
---14. Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles.
+--13. Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles.
 
 SELECT name FROM casting JOIN actor ON actorid=actor.id
 	WHERE ord = 1
-		GROUP BY actor.id
+		GROUP BY actor.id, name
 			HAVING COUNT(name) >= 30
 				ORDER BY name;
 
---15. List the films released in the year 1978 ordered by the number of actors in the cast.
+--14. List the films released in the year 1978 ordered by the number of actors in the cast.
 
 SELECT title, COUNT(actorid) FROM movie JOIN casting ON movie.id=movieid
 	WHERE yr = 1978
-		GROUP BY movieid
+		GROUP BY movieid, title
 			ORDER BY COUNT(actorid) DESC;
 
---16. List all the people who have worked with 'Art Garfunkel'.
+--15. List all the people who have worked with 'Art Garfunkel'.
 
 SELECT name FROM casting JOIN actor ON actorid=actor.id
 	WHERE movieid IN
-		(SELECT movieid FROM casting WHERE actorid =
-			(SELECT actor.id FROM actor WHERE name = 'Art Garfunkel')
+		(SELECT movieid FROM casting JOIN actor ON actorid=id 
+                        WHERE name = 'Art Garfunkel'
 		)
 			AND name <> 'Art Garfunkel';
